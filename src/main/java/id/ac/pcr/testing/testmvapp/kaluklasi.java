64a7d0781c4ca91a828eddd8c6123667a5b6e96a
Isaninside <muhammadihsanz@gmail.com>
2013-04-02 09:45:28 +0700

    Tambah Kalkulasi.java

---
+++ src/main/java/id/ac/pcr/testing/testmvapp/kaluklasi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.pcr.testing.testmvapp;
+
+/**
+ *
+ * @author isaninside
+ */
+public class kaluklasi {
+    private int b;
+    int a = 5+5;    
+}
