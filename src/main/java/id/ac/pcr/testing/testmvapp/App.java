64ef65355a1e603b2b129f94b5f7ee4aa9d07c2d
Isaninside <muhammadihsanz@gmail.com>
2013-04-02 08:27:07 +0700

    Update file App Java Penambahan Private Integer Normal

---
+++ src/main/java/id/ac/pcr/testing/testmvapp/App.java
@@ -0,0 +1,14 @@
+package id.ac.pcr.testing.testmvapp;
+
+/**
+ * Hello Jang!
+ *
+ */
+public class App 
+{
+    private int umur;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
