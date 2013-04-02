8cb2e5cd41df268b7b69a9ba86459ffcca9e7ca2
Isaninside <muhammadihsanz@gmail.com>
2013-04-02 09:23:28 +0700

    Modifikasi file App.java

--- src/main/java/id/ac/pcr/testing/testmvapp/App.java
+++ src/main/java/id/ac/pcr/testing/testmvapp/App.java
@@ -9,6 +9,7 @@ public class App
     private int umur;
     public static void main( String[] args )
     {
+        System.out.println("Hello Hell yeah");
         System.out.println( "Hello World!" );
     }
 }
